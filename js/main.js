dfaaae2af09f1f6280496fdc0ab9de61c37514bc
Cooper Kile <cooperk1807@gmail.com>
2021-07-28 18:42:16 +0000

    Added a function for getting images from api.

--- js/main.js
+++ js/main.js
@@ -1,19 +1,21 @@
 var $img = document.querySelector('img');
 var $left = document.querySelector('.left');
 var $right = document.querySelector('.right');
-var xhr = new XMLHttpRequest();
 var picture = 0;
 
 $right.addEventListener('click', nextImg);
 $left.addEventListener('click', prevImg);
-xhr.addEventListener('load', load);
 
-xhr.open('GET', 'https://picsum.photos/v2/list');
-xhr.responseType = 'json';
-function load() {
-  data.response = xhr.response;
+function getImages() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://picsum.photos/v2/list');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+    data.response = xhr.response;
+  });
+  xhr.send();
 }
-xhr.send();
+getImages();
 
 function nextImg(event) {
   if (picture === 29) {
